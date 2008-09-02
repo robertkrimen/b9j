4201bb3f946cb66afe3583976ef10a7dd665d6fa
robertkrimen <robertkrimen@gmail.com>
2008-09-02 14:41:25 -0700

    Smoker reporting

--- test.smoke/source.js
+++ test.smoke/source.js
@@ -48,12 +48,64 @@
         });
     };
 
-    pckg._TestRunner_COMPLETE_EVENT = function(report) {
-        var uri = b9j.uri.location();
+    pckg._TestRunner_COMPLETE_EVENT = function(b9jReport) {
 
+        var uri = b9j.uri.location();
+        
         if (uri.query().get().b9jSmoke) {
+
+            var $ua = YAHOO.env.ua;
+            var bv, b;
+            var report = {};
+            if (0)
+                ;
+            else if ((bv = $ua.air) > 0) b = "air";
+            else if ((bv = $ua.gecko) > 0) b = "gecko";
+            else if ((bv = $ua.ie) > 0) b = "ie";
+            else if ((bv = $ua.opera) > 0) b = "opera";
+            else if ((bv = $ua.webkit) > 0) b = "webkit";
+            else {
+                b = "unknown";
+                bv = 0;
+            }
+                
+            report.total = b9jReport.total;
+            report.failed = b9jReport.failed;
+            report.reporter_user_agent = navigator.userAgent;
+            report.reporter_user_agent_browser = b;
+            report.reporter_user_agent_browser_version = bv;
+            report.reporter_user_agent_mobile = $ua.mobile;
+            report.reporter_url = uri.toString();
+
             console.log(report);
+            
+            try {
+                $.get("127.0.0.1");
+            }
+            catch (thrown) {
+                console.log(thrown);
+            }
+            var browsersmokeURI;
+            browsersmokeURI = "http://browsersmoke.appspot.com";
+            browsersmokeURI = "http://localhost:8080";
+            console.log("Yoink!");
+//            console.log(
+//                $.post(browsersmokeURI, report, function(){ console.log("Urgh!"); console.log(arguments) }, "json")
+//            );
+                    console.log(browsersmokeURI);
+             $.ajax({
+               url: browsersmokeURI,
+               data: report,
+               success: function() {
+                    console.log(arguments);
+                },
+               error: function() {
+                    console.log(arguments);
+                },
+             });
+
         }
+
     };
 
 }());
