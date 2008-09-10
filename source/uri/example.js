15d4adca2a768d0f16f65970cb94ec9732d77dff
robertkrimen <robertkrimen@gmail.com>
2008-09-09 17:59:26 -0700

    b9j.uri example styling

--- source/uri/example.js
+++ source/uri/example.js
@@ -2,12 +2,12 @@ var uriSourceHistory = [];
 var uri = new b9j.uri.URI();
 
 function example_addHistory(uri, source) {
-    var html =  "<div class=\"yui-g\"><div class=\"yui-u first\">" +
+    var html =  "<div class=\"yui-g\ uri-source-history-line\"><div class=\"yui-u first uri-history\">" +
                 uri +
-                "</div><div class=\"yui-u\">" + 
+                "</div><div class=\"yui-u source-history\"><pre>" + 
                 source +
-                "</div></div>";
-    $("#uri-source-history").append(html);
+                "</pre></div></div>";
+    $("#uri-source-history").prepend(html);
 }
 
 function example_update() {
@@ -18,7 +18,8 @@ function uri_function(fn, arguments_, fn_name) {
     if (! fn_name)
         fn_name = fn;
     uri[fn].apply(uri, arguments_);
-    example_addHistory(uri, "uri." + fn_name + "( " + Array.prototype.slice.call(arguments_).join(",") + " )");
+    var arguments_string = arguments_[0] || arguments_.length > 1 ? " " + Array.prototype.slice.call(arguments_).join(",") + " " : "";
+    example_addHistory(uri, "uri." + fn_name + "(" + arguments_string + ");");
     example_update();
 }
 
@@ -28,15 +29,27 @@ $(document).ready(function(){
         arguments_ = [ ];
         uri_function("up", arguments_, "parent"); 
     });
+
     $("#uri-button-child").bind("click", function(){
         arguments_ = [ $("#uri-input-child").val() ];
         uri_function("down", arguments_, "child"); 
+    });
+
+    $("#uri-button-scheme").bind("click", function(){
+        arguments_ = [ $("#uri-input-scheme").val() ];
+        uri_function("scheme", arguments_);
+    });
+
+    $("#uri-button-scheme-ftp").bind("click", function(){
+        arguments_ = [ "ftp" ];
+        uri_function("scheme", arguments_); 
     });
 
     $("#uri-button-scheme-http").bind("click", function(){
         arguments_ = [ "http" ];
         uri_function("scheme", arguments_); 
     });
+
     $("#uri-button-scheme-https").bind("click", function(){
         arguments_ = [ "https" ];
         uri_function("scheme", arguments_); 
